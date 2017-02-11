6bbcf542535584526b451565b48ac02988b6367e
panforrest <guoqianp@gmail.com>
2017-02-11 13:49:04 -0500

    working on dropzone

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-
+import Dropzone from 'react-dropzone'
 
 class Feed extends Component {
 
 	render(){
 		return(
 			<div>
-                Feed Component
+                <h2>Current Feed</h2>
+
+
+                <div>
+                    <h4>Upload</h4>
+
+                    <Dropzone />
+
+                </div>
 			</div>
 
 
