74b90db029ef6c07bcc47e8be15c6891b72e4099
panforrest <guoqianp@gmail.com>
2017-02-11 13:41:27 -0500

    3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Feed } from './Components'
 
 class App extends Component {
 
@@ -7,6 +8,7 @@ class App extends Component {
     	return (
     		<div>
     		     React App
+    		     <Feed />
 
     		</div>
 
